7c924e62a864db7d09902960ccaf61ec599045d1
yujian <yujian@ysten.com>
2021-02-06 15:51:25 +0800

    添加测试jenkins的代码

--- src/test/java/com/hogwarts/testcase/ApiTest.java
+++ src/test/java/com/hogwarts/testcase/ApiTest.java
@@ -17,7 +17,7 @@ public class ApiTest {
     private String baseUrl = "";
     private String expectCityName = "";
 
-    //添加测试jenkins代码11111
+    //添加测试jenkins代码111111
 
     /**
      * Load config file from the environment path user.home.
